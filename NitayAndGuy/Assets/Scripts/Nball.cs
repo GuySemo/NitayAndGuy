0ae21bddc04f393bee2fac06c2c1c26e01410e06
GuySemo <supersemoil@gmail.com>
2023-12-13 20:50:52 +0200

    ChickenBeta1

--- NitayAndGuy/Assets/Scripts/Nball.cs
+++ NitayAndGuy/Assets/Scripts/Nball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Nball : MonoBehaviour
 {
     [SerializeField] float sizeSpeed=100;
+    bool isTouching = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +15,35 @@ public class Nball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.root.localScale = new Vector3(gameObject.transform.localScale.x *(sizeSpeed-1)/sizeSpeed,gameObject.transform.localScale.y*(sizeSpeed - 1) / sizeSpeed, 1);
+        //Smallning
+        gameObject.transform.root.localScale = 
+            new Vector3( gameObject.transform.localScale.x *(sizeSpeed-1)/sizeSpeed
+            ,  gameObject.transform.localScale.y*(sizeSpeed - 1) / sizeSpeed, 1);
+
+        //Destroy When Too Small
         if (gameObject.transform.root.localScale.x<0.02)
         {
             Destroy(gameObject);
         }
+
+        //Hit Enemy
+        if (gameObject.transform.root.localScale.x < 1 && isTouching)
+        {
+            Debug.Log("Hit");
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collided");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isTouching = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTouching = false;
     }
 
 }
