dfd7be28ed41b05e1b89548a2e5b139588dead30
GuySemo <supersemoil@gmail.com>
2023-12-22 21:40:23 +0200

    Meat

--- NitayAndGuy/Assets/Scripts/Nball.cs
+++ NitayAndGuy/Assets/Scripts/Nball.cs
@@ -8,13 +8,14 @@ public class Nball : MonoBehaviour
     public bool isTouching = false;
     GameObject objectHit;
     [SerializeField] GameObject boomEffect;
+    [SerializeField] GameObject boom2Effect;
 
     //hit closeness
     [SerializeField] float hitCloseness = 0.5f;
 
     //RandomSpin
     float spinDir;
-    // Start is called before the first frame update
+
     void Start()
     {
         spinDir = Random.Range(-720, 720);
@@ -24,7 +25,9 @@ public class Nball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Spinning
         transform.Rotate(0, 0, spinDir * Time.deltaTime);
+
         //Destroy When Too Small
         if (gameObject.transform.root.localScale.x<0.10)
         {
@@ -71,6 +74,7 @@ public class Nball : MonoBehaviour
             gameObject.transform.root.localScale.x > (hitCloseness - hitCloseness / 2) && other.tag == "Enemy")
         {
             Instantiate(boomEffect,  other.gameObject.transform.position, Quaternion.Euler(90, 0, 0));
+            Instantiate(boom2Effect, other.gameObject.transform.position, Quaternion.Euler(90, 0, 0));
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
