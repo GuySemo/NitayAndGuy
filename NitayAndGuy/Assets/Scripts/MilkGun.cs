21a2bd4fbe5d0872b955581c4d73f3340c3236a4
mou002 <150001523+mou002@users.noreply.github.com>
2024-04-10 10:34:12 +0300

    hi guy

--- NitayAndGuy/Assets/Scripts/MilkGun.cs
+++ NitayAndGuy/Assets/Scripts/MilkGun.cs
@@ -21,7 +21,7 @@ public class MilkGun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        timeOfUse = maxCharge;
     }
 
     // Update is called once per frame
